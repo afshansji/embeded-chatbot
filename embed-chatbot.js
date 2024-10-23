c0d02c57d699ea6cac48887ccf45cc6158b4307c
afshansji <afshan.khan@sjinnovation.com>
2024-10-23 18:45:44 +0530

    changed chatbot image

--- embed-chatbot.js
+++ embed-chatbot.js
@@ -6,7 +6,7 @@
     container.innerHTML = `
         <div id="assistant-embed-container">
            <div id="chatbot-icon" style="position:fixed;bottom:40px;right:70px;width:200px;height:200px;display:flex;align-items:center;justify-content:center;cursor:pointer;animation:bounce 2s infinite;">
-              <img src="https://raw.githubusercontent.com/afshansji/embeded-chatbot/main/195-removebg-preview.png" alt="Chatbot" style="width:200px;height:200px;object-fit:cover;" />
+              <img src="https://github.com/afshansji/embeded-chatbot/blob/main/Bot%201.png" alt="Chatbot" style="width:200px;height:200px;object-fit:cover;" />
            </div>
            <div id="assistant-embed" style="position:fixed;bottom:20px;right:20px;width:750px;height:530px;border:1px solid #ccc;border-radius:10px;display:none;">
               <div style="display:flex;justify-content:space-between;align-items:center;padding:10px;background-color:#f0f0f0;border-top-left-radius:10px;border-top-right-radius:10px;">
